8cd8c710052deacd51a7e5c30812f9272f307aab
Priyanka <priyanka.mydesire@gmail.com>
2023-04-13 17:03:42 +0530

    Collision detection between the rectangles.

--- script.js
+++ script.js
@@ -98,6 +98,8 @@ window.addEventListener('load', function(){
             this.x = this.game.width;
             this.speedX = Math.random() * -1.5 -0.5;
             this.markedForDeletion = false;
+            this.lives = 5;
+            this.score = this.lives;
         }
         update(){
             this.x +=this.speedX;
@@ -106,6 +108,8 @@ window.addEventListener('load', function(){
         draw(context){
             context.fillStyle = 'red';
             context.fillRect(this.x, this.y, this.width, this.height);
+            context.font = '20px Helvetica';
+            context.fillText(this.lives, this.x, this.y);
         }
     }
     class Angler1 extends Enemy{
@@ -167,6 +171,19 @@ window.addEventListener('load', function(){
                     }
                     this.enemies.forEach(enemy=>{
                         enemy.update();
+                        if (this.checkCollision(this.player, enemy)) {
+                            enemy.markedForDeletion = true;
+                        }
+                        this.player.projectiles.forEach(projectile =>{
+                            if (this.checkCollision(projectile, enemy)) {
+                                enemy.lives--;
+                                projectile.markedForDeletion = true;
+                                if (enemy.lives <= 0) {
+                                    enemy.markedForDeletion = true;
+                                    this.score += enemy.score;
+                                }
+                            }
+                        })
                     });
                     this.enemies = this.enemies.filter(enemy => !enemy.markedForDeletion);
                     if (this.enemyTimer > this.enemyInterval && !this.gameOver) {
@@ -184,6 +201,14 @@ window.addEventListener('load', function(){
         addEnemy(){
             this.enemies.push(new Angler1(this));
             console.log(this.enemies);
+        }
+        checkCollision(rect1, rect2){
+            return(
+                rect1.x < rect2.x + rect2.width &&
+                rect1.x + rect1.width > rect2.x &&
+                rect1.y < rect2.y + rect2.height &&
+                rect1.height + rect1.y > rect2.y
+            )
         }
     }
 
