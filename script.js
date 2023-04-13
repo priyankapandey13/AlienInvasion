1f2d6fc2721ba9ba13950cf24b0c7241dd08a6d9
Priyanka <priyanka.mydesire@gmail.com>
2023-04-13 16:42:28 +0530

    enemies enters from right add up and who cross gets deleted

--- script.js
+++ script.js
@@ -93,8 +93,30 @@ window.addEventListener('load', function(){
     }
 
     class Enemy{ // Handle different enemy types //
-        dfv
+        constructor(game){
+            this.game = game;
+            this.x = this.game.width;
+            this.speedX = Math.random() * -1.5 -0.5;
+            this.markedForDeletion = false;
+        }
+        update(){
+            this.x +=this.speedX;
+            if(this.x + this.width < 0) this.markedForDeletion = true;
+        }
+        draw(context){
+            context.fillStyle = 'red';
+            context.fillRect(this.x, this.y, this.width, this.height);
+        }
     }
+    class Angler1 extends Enemy{
+        constructor(game){
+            super(game);
+            this.width = 228 * 0.2;
+            this.height = 169 * 0.2;
+            this.y = Math.random() * (this.game.height * 0.9 - this.height);
+        }
+    }
+
 
     class Layer{ // individual multilayer background layer //
         dfv
@@ -126,10 +148,14 @@ window.addEventListener('load', function(){
             this.input = new InputHandler(this);
             this.ui = new UI(this);
             this.keys = [];
+            this.enemies = [];
+            this.enemyTimer = 0;
+            this.enemyInterval = 1000;
             this.ammo = 10;
             this.maxAmmo = 20;
             this.ammoTimer = 0;
             this.ammoInterval = 500;
+            this.gameOver = false;
         }
         update(deltaTime){
             this.player.update();
@@ -139,10 +165,25 @@ window.addEventListener('load', function(){
                 } else{
                 this.ammoTimer += deltaTime;
                     }
+                    this.enemies.forEach(enemy=>{
+                        enemy.update();
+                    });
+                    this.enemies = this.enemies.filter(enemy => !enemy.markedForDeletion);
+                    if (this.enemyTimer > this.enemyInterval && !this.gameOver) {
+                        this.addEnemy();
+                            this.enemyTimer = 0;
+                    }else{
+                        this.enemyTimer += deltaTime;
+                    }
         }
         draw(context){
             this.player.draw(context);
             this.ui.draw(context);
+            this.enemies.forEach(enemy => enemy.draw(context));
+        }
+        addEnemy(){
+            this.enemies.push(new Angler1(this));
+            console.log(this.enemies);
         }
     }
 
