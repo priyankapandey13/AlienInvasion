b5bf3101113849abbfe4a97b91cdc7349c953c80
Priyanka <priyanka.mydesire@gmail.com>
2023-04-30 15:54:40 +0530

    Created a debug mode

--- script.js
+++ script.js
@@ -13,6 +13,8 @@ window.addEventListener('load', function(){
                     this.game.keys.push(e.key);
                 } else if( e.key === ' '){
                     this.game.player.shootTop();
+                } else if(e.key === 'd'){
+                    this.game.debug = !this.game.debug;
                 }
             });
             window.addEventListener('keyup', e =>{
@@ -82,8 +84,7 @@ window.addEventListener('load', function(){
             }
         }
         draw(context){
-            context.fillStyle = 'black';
-            context.fillRect(this.x, this.y, this.width, this.height);
+            if (this.game.debug) context.strokeRect(this.x, this.y, this.width, this.height);
             context.drawImage(this.image, this.frameX * this.width, this.frameY * this.height, this.width, this.height, this.x, this.y, this.width, this.height);
             this.projectiles.forEach(projectile => {
                 projectile.draw(context);
@@ -216,7 +217,7 @@ window.addEventListener('load', function(){
             context.restore();
         }
     }
-    class Game{ // All logic will come togher inside this //
+    class Game { // All logic will come togher inside this //
         constructor(width, height){
             this.width = width;
             this.height = height;
@@ -238,6 +239,7 @@ window.addEventListener('load', function(){
             this.gameTime = 0;
             this.timeLimit = 5000;
             this.speed = 1;
+            this.debug = true;
         }
         update(deltaTime){
             if (!this.gameOver) this.gameTime += deltaTime;
