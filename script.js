ce59134353b9ede5a51adabdc04ad99bc2b48e3c
Priyanka <priyanka.mydesire@gmail.com>
2023-04-30 15:43:49 +0530

    Hero normal sprite animation is done

--- script.js
+++ script.js
@@ -56,9 +56,13 @@ window.addEventListener('load', function(){
             this.height = 190;
             this.x = 20;
             this.y = 100;
+            this.frameX = 0;
+            this.frameY = 0;
+            this.maxFrame = 37;
             this.speedY = 0;
             this.maxSpeed = 2;
             this.projectiles = [];
+            this.image = document.getElementById('player');
         }
         update(){
             if (this.game.keys.includes('ArrowUp')) this.speedY = -this.maxSpeed;
@@ -71,11 +75,16 @@ window.addEventListener('load', function(){
                 projectile.update();
             });
             this.projectiles = this.projectiles.filter(projectile => !projectile.markedForDeletion);
+            if (this.frameX < this.maxFrame) {
+                this.frameX++;
+            }else{
+                this.frameX = 0;
+            }
         }
         draw(context){
             context.fillStyle = 'black';
             context.fillRect(this.x, this.y, this.width, this.height);
-
+            context.drawImage(this.image, this.frameX * this.width, this.frameY * this.height, this.width, this.height, this.x, this.y, this.width, this.height);
             this.projectiles.forEach(projectile => {
                 projectile.draw(context);
             });
