2a2e24cd16b64a901d5275c22fe9d2a78cd55747
Priyanka <priyanka.mydesire@gmail.com>
2023-05-02 00:43:10 +0530

    Cleanup for the player going out of the screen vertically.

--- script.js
+++ script.js
@@ -74,6 +74,9 @@ window.addEventListener('load', function(){
             else if(this.game.keys.includes('ArrowDown')) this.speedY = this.maxSpeed;
             else this.speedY = 0;
             this.y += this.speedY;
+            // Handle vertical boundaries
+            if(this.y > this.game.height - this.height * 0.5) this.y = this.game.height - this.height * 0.5;
+            else if(this.y < -this.height * 0.5) this.y = -this.height * 0.5;
 
             // Handle projectiles
             this.projectiles.forEach(projectile => {
@@ -145,8 +148,10 @@ window.addEventListener('load', function(){
         draw(context){
             if(this.game.debug) context.strokeRect(this.x, this.y, this.width, this.height);
             context.drawImage(this.image, this.frameX * this.width, this.frameY * this.height, this.width, this.height, this.x, this.y, this.width, this.height);
-            context.font = '20px Helvetica';
-            context.fillText(this.lives, this.x, this.y);
+            if(this.game.debug){
+                context.font = '20px Helvetica';
+                context.fillText(this.lives, this.x, this.y);
+            }
         }
     }
     class Angler1 extends Enemy{
